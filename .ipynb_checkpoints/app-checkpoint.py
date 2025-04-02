f67a5cc9c9290546552cfaebbfbfa7ef8eb0d238
geeta thakur <csegeeta24@gmail.com>
2025-04-02 11:47:31 +0530

    Initial commit

---
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,116 @@
+import streamlit as st
+import pickle
+from streamlit_option_menu import option_menu
+
+# Page Configuration
+st.set_page_config(page_title="Disease Prediction", page_icon="⚕️")
+
+# Hide Streamlit add-ons
+hide_st_style = """
+         <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        header {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+# Adding Background Image with Reduced Overlay
+background_image_url = "https://content.presspage.com/uploads/2170/475328e9-33c6-4b3e-bfae-c0c09c4f7ee1/1920_gettyimages-ai-clinic-copy.jpeg?10000"
+
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] {{
+    background-image: url({background_image_url});
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+}}
+[data-testid="stAppViewContainer"]::before {{
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.4); /* Reduced darkness */
+}}
+</style>
+"""
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+# Load Models Safely
+models = {
+    'heart_disease': pickle.load(open('Models/heart_disease_model.sav', 'rb'))
+}
+
+# Dropdown for Disease Selection
+selected = st.selectbox(
+    'Disease to Predict',   
+    [
+     'Heart Disease Prediction',
+    ]
+)
+
+# Function to Create Input Fields
+def display_input(label, key, tooltip, type="text"):
+    if type == "text":
+        return st.text_input(label, key=key, help=tooltip)
+    elif type == "number":
+        return st.number_input(label, key=key, help=tooltip, step=1)
+   
+# # Heart Disease Prediction Page
+# if selected == 'Heart Disease Prediction':
+#     st.title('Heart Disease Prediction')
+#     st.write("Enter the following details to predict heart disease:")
+    
+#     age = display_input('Age', 'Enter age of the person', 'age', 'number')
+#     sex = display_input('Sex (1 = Male; 0 = Female)', 'Enter sex of the person', 'sex', 'number')
+#     cp = display_input('Chest Pain Type (0-3)', 'Enter chest pain type', 'cp', 'number')
+#     trestbps = display_input('Resting Blood Pressure', 'Enter resting blood pressure', 'trestbps', 'number')
+#     chol = display_input('Cholesterol Level', 'Enter serum cholesterol', 'chol', 'number')
+#     fbs = display_input('Fasting Blood Sugar (>120 mg/dl: 1, else 0)', 'Enter fasting blood sugar', 'fbs', 'number')
+#     restecg = display_input('Resting ECG (0-2)', 'Enter resting ECG results', 'restecg', 'number')
+#     thalach = display_input('Max Heart Rate', 'Enter maximum heart rate', 'thalach', 'number')
+#     exang = display_input('Exercise Induced Angina (1 = Yes; 0 = No)', 'Enter exercise induced angina', 'exang', 'number')
+#     oldpeak = display_input('ST Depression', 'Enter ST depression value', 'oldpeak', 'number')
+#     slope = display_input('Slope of Peak (0-2)', 'Enter slope value', 'slope', 'number')
+#     ca = display_input('Major Vessels (0-3)', 'Enter number of major vessels', 'ca', 'number')
+#     thal = display_input('Thal (0 = normal; 1 = fixed defect; 2 = reversible defect)', 'Enter thal value', 'thal', 'number')
+    
+#     heart_diagnosis = ''
+#     if st.button('Heart Disease Test Result'):
+#         heart_prediction = models['heart_disease'].predict([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]])
+#         heart_diagnosis = 'The person has heart disease' if heart_prediction[0] == 1 else 'The person does not have heart disease'
+#         st.success(heart_diagnosis)
+   
+   # Heart Disease Prediction Page
+if selected == 'Heart Disease Prediction':
+    st.title('Heart Disease Prediction')
+    st.write("Enter the following details to predict heart disease:")
+    
+    Age = display_input('Age', 'Enter age of the person', 'age', 'number')
+    Sex = display_input('Sex (1 = Male; 0 = Female)', 'Enter sex of the person', 'sex', 'number')
+    ChestPainType = display_input('Chest Pain Type (0-3)', 'Enter chest pain type', 'cp', 'number')
+    RestingBloodPressure = display_input('Resting Blood Pressure', 'Enter resting blood pressure', 'trestbps', 'number')
+    CholesterolLevel = display_input('Cholesterol Level', 'Enter serum cholesterol', 'chol', 'number')
+    FastingBloodSugar = display_input('Fasting Blood Sugar (>120 mg/dl: 1, else 0)', 'Enter fasting blood sugar', 'fbs', 'number')
+    RestingECG = display_input('Resting ECG (0-2)', 'Enter resting ECG results', 'restecg', 'number')
+    MaxHeartRate = display_input('Max Heart Rate', 'Enter maximum heart rate', 'thalach', 'number')
+    ExerciseInducedAngina = display_input('Exercise Induced Angina (1 = Yes; 0 = No)', 'Enter exercise induced angina', 'exang', 'number')
+    STDepression = display_input('ST Depression', 'Enter ST depression value', 'oldpeak', 'number')
+    SlopeofPeak = display_input('Slope of Peak (0-2)', 'Enter slope value', 'slope', 'number')
+    MajorVessels = display_input('Major Vessels (0-3)', 'Enter number of major vessels', 'ca', 'number')
+    Thal = display_input('Thal (0 = normal; 1 = fixed defect; 2 = reversible defect)', 'Enter thal value', 'thal', 'number')
+    
+    heart_diagnosis = ''
+    if st.button('Heart Disease Test Result'):
+        heart_prediction = models['heart_disease'].predict([[Age, Sex, ChestPainType, RestingBloodPressure, CholesterolLevel, 
+                                                     FastingBloodSugar, RestingECG, MaxHeartRate, ExerciseInducedAngina, 
+                                                     STDepression, SlopeofPeak, MajorVessels, Thal]])
+
+        # heart_prediction = models['heart_disease'].predict([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]])
+        heart_diagnosis = 'The person has heart disease' if heart_prediction[0] == 1 else 'The person does not have heart disease'
+        st.success(heart_diagnosis)
+                      
