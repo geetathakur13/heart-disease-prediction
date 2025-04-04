7580ba1ae01e22cebbd3bc6ac3f6b4ec413140ed
geeta thakur <csegeeta24@gmail.com>
2025-04-04 09:33:02 +0530

    Updated UI and fixed mobile visibility for mobile devices

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ page_bg_img = f"""
     left: 0;
     width: 100%;
     height: 100%;
-    background-color: rgba(0, 0, 0, 0.4); /* Reduced darkness */
+    background-color: rgba(0, 0, 0, 0.7); /* Reduced darkness */
 }}
 </style>
 """
@@ -55,53 +55,7 @@ def display_input(label, key, tooltip, type="text"):
 
 if selected == 'Heart Disease Prediction':
     st.title('Heart Disease Prediction')
-    st.write("Enter the following details to predict heart disease:")
-    
-    # age = display_input('Age', 'Enter age of the person', 'age', 'number')
-    # sex = display_input('Sex (1 = Male; 0 = Female)', 'Enter sex of the person', 'sex', 'number')
-    # cp = display_input('Chest Pain Type (0-3)', 'Enter chest pain type', 'cp', 'number')
-    # trestbps = display_input('Resting Blood Pressure', 'Enter resting blood pressure', 'trestbps', 'number')
-    # chol = display_input('Cholesterol Level', 'Enter serum cholesterol', 'chol', 'number')
-    # fbs = display_input('Fasting Blood Sugar (>120 mg/dl: 1, else 0)', 'Enter fasting blood sugar', 'fbs', 'number')
-    # restecg = display_input('Resting ECG (0-2)', 'Enter resting ECG results', 'restecg', 'number')
-    # thalach = display_input('Max Heart Rate', 'Enter maximum heart rate', 'thalach', 'number')
-    # exang = display_input('Exercise Induced Angina (1 = Yes; 0 = No)', 'Enter exercise induced angina', 'exang', 'number')
-    # oldpeak = display_input('ST Depression', 'Enter ST depression value', 'oldpeak', 'number')
-    # slope = display_input('Slope of Peak (0-2)', 'Enter slope value', 'slope', 'number')
-    # ca = display_input('Major Vessels (0-3)', 'Enter number of major vessels', 'ca', 'number')
-    # thal = display_input('Thal (0 = normal; 1 = fixed defect; 2 = reversible defect)', 'Enter thal value', 'thal', 'number')
-# Age = display_input('Age', 'Enter age of the person', 'age', 'number')
-# Sex = display_input('Sex (1 = Male; 0 = Female)', 'Enter sex of the person', 'sex', 'number')
-# ChestPainType = display_input('Chest Pain Type (0-3)', 'Enter chest pain type', 'cp', 'number')
-# RestingBloodPressure = display_input('Resting Blood Pressure', 'Enter resting blood pressure', 'trestbps', 'number')
-# CholesterolLevel = display_input('Cholesterol Level', 'Enter serum cholesterol', 'chol', 'number')
-# FastingBloodSugar = display_input('Fasting Blood Sugar (>120 mg/dl: 1, else 0)', 'Enter fasting blood sugar', 'fbs', 'number')
-# RestingECG = display_input('Resting ECG (0-2)', 'Enter resting ECG results', 'restecg', 'number')
-# MaxHeartRate = display_input('Max Heart Rate', 'Enter maximum heart rate', 'thalach', 'number')
-# ExerciseInducedAngina = display_input('Exercise Induced Angina (1 = Yes; 0 = No)', 'Enter exercise induced angina', 'exang', 'number')
-# STDepression = display_input('ST Depression', 'Enter ST depression value', 'oldpeak', 'number')
-# SlopeofPeak = display_input('Slope of Peak (0-2)', 'Enter slope value', 'slope', 'number')
-# MajorVessels = display_input('Major Vessels (0-3)', 'Enter number of major vessels', 'ca', 'number')
-# Thal = display_input('Thal (0 = normal; 1 = fixed defect; 2 = reversible defect)', 'Enter thal value', 'thal', 'number')
-
-    
-#     # heart_diagnosis = ''
-#     # if st.button('Heart Disease Test Result'):
-#     #     heart_prediction = models['heart_disease'].predict([[Age, Sex, ChestPainType, RestingBloodPressure, CholesterolLevel, 
-#     #                                                  FastingBloodSugar, RestingECG, MaxHeartRate, ExerciseInducedAngina, 
-#     #                                                  STDepression, SlopeofPeak, MajorVessels, Thal]])
-
-#     #     # heart_prediction = models['heart_disease'].predict([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]])
-#     #     heart_diagnosis = 'The person has heart disease' if heart_prediction[0] == 1 else 'The person does not have heart disease'
-#     #     st.success(heart_diagnosis)
-#     heart_diagnosis = ''
-#     if st.button('Heart Disease Test Result'):
-#         heart_prediction = models['heart_disease'].predict([[Age, Sex, ChestPainType, RestingBloodPressure, CholesterolLevel, FastingBloodSugar, RestingECG, MaxHeartRate, ExerciseInducedAngina, STDepression, SlopeofPeak, MajorVessels, Thal]])
-
-#         # heart_prediction = models['heart_disease'].predict([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]])
-#         heart_diagnosis = 'The person has heart disease' if heart_prediction[0] == 1 else 'The person does not have heart disease'
-#         st.success(heart_diagnosis)
-                      
+    st.write("Enter the following details to predict heart disease:")          
 if selected == 'Heart Disease Prediction':
     st.title('Heart Disease Prediction')
     st.write("Enter the following details to predict heart disease:")
